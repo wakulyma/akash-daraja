0616012c00ce4c0c9a10b83d400d80740c8ce3c5
ian.dev <nmwanik111@gmail.com>
2024-05-17 18:34:51 +0300

    fixed cron error

--- src/cronJobs/dbDumpCron.ts
+++ src/cronJobs/dbDumpCron.ts
@@ -1,7 +1,7 @@
 import { CronJob } from "cron";
 import { dumpDB } from "../services/storage/jackal";
 
-export const backupDBStateCron = new CronJob(`0 0/15 0/7 * *`, async () => {
+export const backupDBStateCron = new CronJob(`0 0/15 1/7 * *`, async () => {
   console.log("Running db backup");
   await dumpDB();
 });
